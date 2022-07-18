cf5495263669aaae3685882f2eed011b48380d29
monica <monicasastudy@gmail.com>
2022-07-18 17:33:54 +0200

    auth routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css';
+import './App.css'
+import AppRoutes from './routes/AppRoutes'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>VAAAAA</h1>
+        <AppRoutes />
       </header>
     </div>
   );
