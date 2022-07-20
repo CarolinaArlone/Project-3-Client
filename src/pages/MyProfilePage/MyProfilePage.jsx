f151d5920c77d826d9476ec18196c819671f929b
CarolinaArlone <carolinaarlone@gmail.com>
2022-07-20 21:13:15 +0200

    Fixed package jason problem

--- src/pages/MyProfilePage/MyProfilePage.jsx
+++ src/pages/MyProfilePage/MyProfilePage.jsx
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from 'react-bootstrap'
-import Maps from '../../components/Maps/Maps'
+// import Maps from '../../components/Maps/Maps'
 import Loader from '../../components/Loader/Loader'
 
 
@@ -15,13 +15,13 @@ const MyProfilePage = () => {
 
                 </Col>
 
-                <Maps
-                    // googleMapURL="http://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDLf7y6k5PGx_cjdDPGxNq8wy7UDjKTajo"
-                    // containerElement={<div style={{ height: '400px' }} />}
-                    // mapElement={<div style={{ height: '100%' }} />}
-                    // loadingElement={<Loader />}
+                {/* <Maps
+                    googleMapURL="http://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDLf7y6k5PGx_cjdDPGxNq8wy7UDjKTajo"
+                    containerElement={<div style={{ height: '400px' }} />}
+                    mapElement={<div style={{ height: '100%' }} />}
+                    loadingElement={<Loader />}
 
-                />
+                /> */}
             </Row>
         </Container>
 
