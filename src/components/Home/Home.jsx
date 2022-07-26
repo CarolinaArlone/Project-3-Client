459fc41bad27e5362fa7958bee3a9def474a2710
monica <monicasastudy@gmail.com>
2022-07-26 15:16:14 +0200

    footer-navbar-home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,25 +7,27 @@ const Home = () => {
         <>
             <div className='header'>
                 <img src='https://res.cloudinary.com/djs7qv2pt/image/upload/v1658734774/fondogoto_fme6si.webp' alt="home"></img>
-            </div>
-            <Container>
-                <Row>
-                    <Col md={6}>
-                        <div className='left-content'>
-                            <p className="stay-home">TE LLEVO!</p>
-                            <h1>Te lleva y tu disfrutas del viaje.</h1>
-                            <br />
-                            <p className="para">
-                                Te-Llevo te ofrece todos los vehículos que necesitas para moverte libremente
-                            </p>
-                        </div>
-                    </Col>
 
-                    <Col md={6}>
-                        <img className='img-second' src="https://res.cloudinary.com/djs7qv2pt/image/upload/v1658670846/vvjcj2edhbfytiecmuh6.png" alt="image" />
-                    </Col>
-                </Row>
-            </Container>
+                <Container>
+                    <Row>
+                        <Col md={6}>
+                            <div className='left-content'>
+                                <p className="stay-home">TE LLEVO!</p>
+                                <h1>Te lleva y tu disfrutas del viaje.</h1>
+                                <br />
+                                <p className="para">
+                                    Te-Llevo te ofrece todos los vehículos que necesitas para moverte libremente
+                                </p>
+                            </div>
+                        </Col>
+
+                        <Col md={6}>
+                            <img className='img-second' src="https://res.cloudinary.com/djs7qv2pt/image/upload/v1658670846/vvjcj2edhbfytiecmuh6.png" alt="image" />
+                        </Col>
+                    </Row>
+                </Container>
+
+            </div>
 
 
 
